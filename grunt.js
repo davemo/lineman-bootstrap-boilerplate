cdf6016a40b5d3da46af9467b75ce529cbc107c2
David Mosher <davidmosher@gmail.com>
2013-01-04 16:27:53 -0600

    Updates grunt config to use latest lineman stock

--- grunt.js
+++ grunt.js
@@ -2,25 +2,13 @@
 module.exports = function(grunt) {
 
   // STOCK LINEMAN
-
-  var _ = grunt.utils._,
-      server = require(process.cwd() + '/config/server.js'),
-      appTasks = require(process.cwd() + '/config/application.js').appTasks;
-
-  grunt.loadTasks('tasks');
-  grunt.loadNpmTasks('grunt-contrib');
-  grunt.loadNpmTasks('lineman');
-
-  grunt.registerTask('default', _.union(appTasks.common, appTasks.dist).join(' '));
-  grunt.registerTask('run', _.union(appTasks.common, appTasks.watch).join(' '));
-
-  grunt.task.run('configure');
+  require('lineman').config.grunt.run(grunt);
 
   // MY LINEMAN CUSTOMIZATIONS
 
   // I like to use a custom express server for stubbing server actions
   grunt.registerTask('server', 'Custom Express Server', function() {
-    grunt.log.writeln('Starting express web server in "./generated" on port 3000');
-    server.listen(3000);
+    grunt.log.writeln('Starting express web server in "./generated" on port 8000');
+    require(process.cwd() + '/config/server.js').listen(process.env.WEB_PORT || 8000);
   });
 };
